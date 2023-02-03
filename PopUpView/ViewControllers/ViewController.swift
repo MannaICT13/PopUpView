d848fb3b6ecdbc5c1d9ac4548c2579ee0f7a04bd
Md Khaled Hasan Manna <manna@sharetrip.net>
2023-02-03 23:27:09 +0600

    create alert view

--- PopUpView/ViewControllers/ViewController.swift
+++ PopUpView/ViewControllers/ViewController.swift
@@ -11,9 +11,35 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    private weak var alerView: UIView?
+    
+    private func showAlertPopupView() {
+        let popupView = PopupView.instantiate()
+        alerView = popupView
+        
+        popupView.callback.didConfirm = {[weak self] in
+            self?.alerView?.removeFromSuperview()
+            self?.navigationItem.setHidesBackButton(false, animated: false)
+  
+        }
+        
+        popupView.callback.didClose = {[weak self] in
+            self?.alerView?.removeFromSuperview()
+            self?.navigationItem.setHidesBackButton(false, animated: false)
+        }
+        
+        popupView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(popupView)
+        let constraints = [
+            popupView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            popupView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            popupView.bottomAnchor .constraint(equalTo: view.bottomAnchor),
+            popupView.topAnchor.constraint(equalTo: view.topAnchor)
+        ]
+        navigationItem.setHidesBackButton(true, animated: false)
+        NSLayoutConstraint.activate(constraints)
+    }
 }
 
