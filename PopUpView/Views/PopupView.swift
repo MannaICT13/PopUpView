85a449bd05c1e2ff7bfa59ee8274742f9d08687e
Md Khaled Hasan Manna <manna@sharetrip.net>
2023-02-03 23:15:43 +0600

    create outlets and actions

--- PopUpView/Views/PopupView.swift
+++ PopUpView/Views/PopupView.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import UIKit
+
+class PopupView: UIView {
+    @IBOutlet weak var contentView: UIView! {
+        didSet {
+            contentView.backgroundColor = UIColor.black
+            contentView.alpha = 0.3
+        }
+    }
+    
+    @IBAction private func confirmButtonAction(_ sender: UIButton) {
+    }
+    
+    @IBAction private func closeButtonAction(_ sender: UIButton) {
+    }
+}
