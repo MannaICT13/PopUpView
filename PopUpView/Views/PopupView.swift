0db63bf0767002007523eadfe9b0211f4d7ab575
Md Khaled Hasan Manna <manna@sharetrip.net>
2023-02-03 23:26:47 +0600

    create callbacks

--- PopUpView/Views/PopupView.swift
+++ PopUpView/Views/PopupView.swift
@@ -8,17 +8,27 @@
 import Foundation
 import UIKit
 
-class PopupView: UIView {
+extension PopupView {
+    class Callback {
+        var didConfirm: () -> Void = { }
+        var didClose: () -> Void = { }
+    }
+}
+
+class PopupView: UIView, NibBased {
     @IBOutlet weak var contentView: UIView! {
         didSet {
             contentView.backgroundColor = UIColor.black
             contentView.alpha = 0.3
         }
     }
+    let callback = Callback()
     
     @IBAction private func confirmButtonAction(_ sender: UIButton) {
+        callback.didConfirm()
     }
     
     @IBAction private func closeButtonAction(_ sender: UIButton) {
+        callback.didClose()
     }
 }
